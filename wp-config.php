de5372e3120b7b3feeb5d8d2d8b76134f33dcdec
melaniegrigg <melanie.grigger@gmail.com>
2020-11-13 07:42:45 -0500

    edit blog layout and add wpforms plugin

--- wp-config.php
+++ wp-config.php
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'H>yg;EmhJ1!><GUcNA5A1ZB2Mc`~Ww.&]r&F@gpYgiJ3S<mlk]sls@]U)g3c`CWQ' );
-define( 'SECURE_AUTH_KEY',  '])B>APn$0*jc+8/RrydYNytttyQRh&Os^1I}c*qcc=.&|Fr0~xAo#p&*9zdKKV{Z' );
-define( 'LOGGED_IN_KEY',    '%xa1:RAgz*t22%A$o#T00RTMXJc>+ !8$NKRU6u1!Ld53;oc{({,ha-4:L(G0?Xn' );
-define( 'NONCE_KEY',        'gZ5AE5FK=@IEq==6o[w/P$Wt*=5`p6O:.y!pn1ZhG#?f<:il)KGlRwMO[DXN<8q+' );
-define( 'AUTH_SALT',        '~ Gso_=O>cOG:9J+%_B>Pyft?lY/J$8GMz}-:alkt~GK+GFo]|s%f{Tq(UatfT$x' );
-define( 'SECURE_AUTH_SALT', '>91{qg^R+O0Ryw`ZRAWI^a~d rN9/0e:K8~L8`Xyn)vJ 2cRpx<XaOE4]HA7m%Kj' );
-define( 'LOGGED_IN_SALT',   '>p=0n!_2i@S2aL$aK*_u|(6({Wz7T3/chj,op5zwB8R{cmh4i{~?2IZGWJcPMS!{' );
-define( 'NONCE_SALT',       'X2;(Nb@0XP7_ZX -b4+1nj|}db$^KTkdhFjp058+okaX(9HEz+/ abnSC:,Kpec&' );
+define('AUTH_KEY',         'FnLdB9<SJwO]B4T_z^%$u0_.]8F!1WM`[wM=%1[d7,+@YSN[`!piJ^WJN_s4:<;L');
+define('SECURE_AUTH_KEY',  '|}_Hc*%2`k<xzqW:6JME|*}k789Po8bN*hgx-7X^V/51s^!`U}0$sW&Od--1IJpS');
+define('LOGGED_IN_KEY',    'X|dV}8v|G8aY(3VPUy1a-e>AGIKgo70OqSgm8tCecoKzE0J7|059n^D<zlxbjw_r');
+define('NONCE_KEY',        '>*R=Q`#ezLsuM{>uHlp!!A2;@|asC4(Fc?XvQ:B2P?%,o8s`rI$q<V~V|ir3A)L)');
+define('AUTH_SALT',        's=cXg+}_z+*=rQE<EhXX_BeLz@;eUX8~y;+-1iOY?2P[N-B++.T}.eaK<rF~>>N#');
+define('SECURE_AUTH_SALT', 'Up>B(n-Ru:)PJOub[;?=t9|Y$sFtp3ITQ>m|[qqGWvlYc<A[{tt-GzBVfUWJqX.R');
+define('LOGGED_IN_SALT',   'Em/L+h-s[*.jt@u.(dVW3jG&!Tcv#_+!hr_w&08bGXu;YTOz!b9;Gu6_=zkI+F@d');
+define('NONCE_SALT',       'E!O1p/*Z!-nyP$>!|gqKOY!hSf4)]+&T !f!:I}-2;J5^slb;LP#Aqhrqt+Hu!6!');
 
 /**#@-*/
 
